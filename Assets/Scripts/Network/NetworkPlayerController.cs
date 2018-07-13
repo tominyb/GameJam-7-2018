5d60d60b0a8d3ca19eacd6f2a92efa3cdf777568
Aleksanteri Hirvonen <aleksanteri.hirvonen@gmail.com>
2018-07-13 21:33:22 +0300

    Setup sending/receiving directions

--- Assets/Scripts/Network/NetworkPlayerController.cs
+++ Assets/Scripts/Network/NetworkPlayerController.cs
@@ -1,39 +1,58 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.Networking;
 
 public class NetworkPlayerController : NetworkBehaviour
 {
+    private readonly Dictionary<KeyCode, Vector2Int> m_commandDirections = new Dictionary<KeyCode, Vector2Int>
+    {
+        { KeyCode.A, Vector2Int.left },
+        { KeyCode.D, Vector2Int.right },
+        { KeyCode.S, Vector2Int.down },
+        { KeyCode.W, Vector2Int.up }
+    };
+
     private void Update()
     {
-        if (isLocalPlayer && Input.GetKeyDown(KeyCode.Space))
+        if (!isLocalPlayer)
         {
-            Notify();
+            return;
+        }
+
+        foreach (var entry in m_commandDirections)
+        {
+            if (Input.GetKeyDown(entry.Key))
+            {
+                NotifyDirection(entry.Value);
+            }
         }
     }
 
-    private void Notify()
+    private void NotifyDirection(Vector2Int direction)
     {
+        // Vector2Int is not supported as an argument to Remote Actions (ClientRpc, Command).
+        // Therefore, directions are sent as Vector2's with proper conversions done in both ends.
         if (isServer)
         {
-            Debug.Log("Notifying clients...");
-            RpcNotifyClients();
+            Debug.Log("Notifying clients: " + direction);
+            RpcNotifyDirectionToClients(direction);
         }
         else
         {
-            Debug.Log("Notifying server...");
-            CmdNotifyServer();
+            Debug.Log("Notifying server: " + direction);
+            CmdNotifyDirectionToServer(direction);
         }
     }
 
     [ClientRpc]
-    private void RpcNotifyClients()
+    private void RpcNotifyDirectionToClients(Vector2 directionFloat)
     {
-        Debug.Log("Server notify received");
+        Debug.Log("Server notify received: " + Vector2Int.RoundToInt(directionFloat));
     }
 
     [Command]
-    private void CmdNotifyServer()
+    private void CmdNotifyDirectionToServer(Vector2 directionFloat)
     {
-        Debug.Log("Client notify received");
+        Debug.Log("Client notify received: " + Vector2Int.RoundToInt(directionFloat));
     }
 }
