ed0a2ab1662f3f97ea9e03484ff935bc1c9789ca
Aleksanteri Hirvonen <aleksanteri.hirvonen@gmail.com>
2018-07-14 01:58:16 +0300

    Implement turn time synchronization over network

---
+++ Assets/Scripts/Network/NetworkTurnManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkTurnManager : NetworkBehaviour
+{
+    [SerializeField] private TurnTimeLeftBar m_turnTimeLeftBar;
+    [SerializeField] private float m_turnTime = 5f;
+    [SerializeField] private float m_timeBetweenTurns = 1f;
+    private float m_turnTimeLeft;
+    private IEnumerator m_clientTimeLeftUpdate;
+
+    private void Start()
+    {
+        if (isServer)
+        {
+            StartCoroutine(HandleTurns());
+        }
+    }
+
+    [Server]
+    private IEnumerator HandleTurns()
+    {
+        for (;;)
+        {
+            yield return new WaitForSeconds(m_timeBetweenTurns);
+            RpcStartTurn(m_turnTime);
+            yield return new WaitForSeconds(m_turnTime);
+            RpcEndTurn();
+        }
+    }
+
+    [ClientRpc]
+    private void RpcStartTurn(float turnTime)
+    {
+        Debug.Log("Turn started.");
+        m_turnTime = turnTime;
+        m_turnTimeLeftBar.TurnTime = m_turnTime;
+        m_turnTimeLeft = turnTime;
+        m_clientTimeLeftUpdate = HandleTurnTimeUpdates();
+        StartCoroutine(m_clientTimeLeftUpdate);
+    }
+
+    [ClientRpc]
+    private void RpcEndTurn()
+    {
+        Debug.Log("Turn ended.");
+        m_turnTimeLeft = 0f;
+        StopCoroutine(m_clientTimeLeftUpdate);
+        m_clientTimeLeftUpdate = null;
+    }
+
+    [Client]
+    private IEnumerator HandleTurnTimeUpdates()
+    {
+        for (;;)
+        {
+            m_turnTimeLeft = Mathf.Max(m_turnTimeLeft - Time.deltaTime, 0.0f);
+            m_turnTimeLeftBar.TurnTimeLeft = m_turnTimeLeft;
+            yield return null;
+        }
+    }
+}
--- Assets/Scripts/UI/TurnTimeLeftBar.cs
+++ Assets/Scripts/UI/TurnTimeLeftBar.cs
@@ -2,32 +2,20 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TurnTimeLeft : MonoBehaviour
+public class TurnTimeLeftBar : MonoBehaviour
 {
     [SerializeField] private Gradient m_sliderColorGradient;
     [SerializeField] private Slider m_slider;
     private Image m_sliderFillImage;
+
+    public float TurnTime { set { m_slider.maxValue = value; } }
+    public float TurnTimeLeft { set { m_slider.value = value; } }
 
     private void Start()
     {
         m_slider = GetComponent<Slider>();
         m_slider.onValueChanged.AddListener(delegate { UpdateSliderColorAccordingToValuePercentage(); });
         m_sliderFillImage = m_slider.fillRect.GetComponent<Image>();
-        StartCoroutine(TestSliderColorChangeAtInterval(1f));
-    }
-
-    private IEnumerator TestSliderColorChangeAtInterval(float interval)
-    {
-        while (true)
-        {
-            RandomizeSliderValue();
-            yield return new WaitForSeconds(interval);
-        }
-    }
-
-    private void RandomizeSliderValue()
-    {
-        m_slider.value = Random.Range(m_slider.minValue, m_slider.maxValue);
     }
 
     private void UpdateSliderColorAccordingToValuePercentage()
