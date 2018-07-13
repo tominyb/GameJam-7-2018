875a8cb78f7fca8696eebb7e449a0a9bcb936ec7
Aleksanteri Hirvonen <aleksanteri.hirvonen@gmail.com>
2018-07-13 21:02:03 +0300

    Setup test client/server notify

---
+++ Assets/Scripts/Network/NetworkPlayerController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkPlayerController : NetworkBehaviour
+{
+    private void Update()
+    {
+        if (isLocalPlayer && Input.GetKeyDown(KeyCode.Space))
+        {
+            Notify();
+        }
+    }
+
+    private void Notify()
+    {
+        if (isServer)
+        {
+            Debug.Log("Notifying clients...");
+            RpcNotifyClients();
+        }
+        else
+        {
+            Debug.Log("Notifying server...");
+            CmdNotifyServer();
+        }
+    }
+
+    [ClientRpc]
+    private void RpcNotifyClients()
+    {
+        Debug.Log("Server notify received");
+    }
+
+    [Command]
+    private void CmdNotifyServer()
+    {
+        Debug.Log("Client notify received");
+    }
+}
