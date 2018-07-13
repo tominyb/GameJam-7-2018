55cd7fbf4c1394504a5dcc22ec3ceebf8673fdfe
Aleksanteri Hirvonen <aleksanteri.hirvonen@gmail.com>
2018-07-13 23:59:21 +0300

    Setup utilities for drawing turn time left bar

---
+++ Assets/Scripts/UI/TurnTimeLeft.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TurnTimeLeft : MonoBehaviour
+{
+    [SerializeField] private Gradient m_sliderColorGradient;
+    [SerializeField] private Slider m_slider;
+    private Image m_sliderFillImage;
+
+    private void Start()
+    {
+        m_slider = GetComponent<Slider>();
+        m_slider.onValueChanged.AddListener(delegate { UpdateSliderColorAccordingToValuePercentage(); });
+        m_sliderFillImage = m_slider.fillRect.GetComponent<Image>();
+        StartCoroutine(TestSliderColorChangeAtInterval(1f));
+    }
+
+    private IEnumerator TestSliderColorChangeAtInterval(float interval)
+    {
+        while (true)
+        {
+            RandomizeSliderValue();
+            yield return new WaitForSeconds(interval);
+        }
+    }
+
+    private void RandomizeSliderValue()
+    {
+        m_slider.value = Random.Range(m_slider.minValue, m_slider.maxValue);
+    }
+
+    private void UpdateSliderColorAccordingToValuePercentage()
+    {
+        m_sliderFillImage.color = m_sliderColorGradient.Evaluate(GetSliderValuePercentage());
+    }
+
+    private float GetSliderValuePercentage()
+    {
+        return (m_slider.value - m_slider.minValue) / (m_slider.maxValue - m_slider.minValue);
+    }
+}
