64d727327d18a2acd9c7ec7d9352b5c3c761ff7c
Aleksanteri Hirvonen <aleksanteri.hirvonen@gmail.com>
2018-07-15 00:13:19 +0300

    Omit unused usings

--- Assets/Scripts/Network/CustomNetworkManager.cs
+++ Assets/Scripts/Network/CustomNetworkManager.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using UnityEngine;
 using UnityEngine.Networking;
 
 public class CustomNetworkManager : NetworkManager
--- Assets/Scripts/Network/NetworkMonster.cs
+++ Assets/Scripts/Network/NetworkMonster.cs
@@ -1,6 +1,5 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
-using System.Collections;
 
 [RequireComponent(typeof(NetworkHealth))]
 public class NetworkMonster : NetworkBehaviour
