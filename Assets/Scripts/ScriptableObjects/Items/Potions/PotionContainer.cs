8067ccb37211b2adaacd77af820f1fb77030baf8
Aleksanteri Hirvonen <aleksanteri.hirvonen@gmail.com>
2018-07-14 15:26:44 +0300

    Add potion utility classes

---
+++ Assets/Scripts/ScriptableObjects/Items/Potions/PotionContainer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace ScriptableObjects {
+
+[CreateAssetMenu(fileName = "PotionContainer", menuName = "ScriptableObjects/PotionContainer", order = 0)]
+public class PotionContainer : ScriptableObject
+{
+    public Potion[] potions;
+
+    public Potion GetRandomPotion()
+    {
+        return potions[Random.Range(0, potions.Length)];
+    }
+}
+
+} // namespace ScriptableObjects
